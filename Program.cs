7c87220489fc1835d7f843d8a0f06f517e88b196
2173710 <2173710@JAC-6SFL7Y3>
2024-02-21 10:42:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jacappdev1_2024_dotnetjets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
