8f06c713492e8e2401e7d1416be6edc10c8f2ce6
Julien <julienhalde9@gmail.com>
2024-04-15 13:19:53 -0400

    Started implementation of ViewInterface and Presenter

--- HomeCalendarWPF/Presenter.cs
+++ HomeCalendarWPF/Presenter.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Calendar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,17 @@ namespace HomeCalendarWPF
 {
     internal class Presenter
     {
+        // Links from view, model to Presenter
+        private readonly ViewInterface view;
+        private readonly HomeCalendar model;
+
+        // Presenter constructor
+        public Presenter(ViewInterface v)
+        {
+            //model = new HomeCalendar(); 
+            view = v;
+        }
+
+
     }
 }
--- HomeCalendarWPF/ViewInterface.cs
+++ HomeCalendarWPF/ViewInterface.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calendar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace HomeCalendarWPF
 {
     internal interface ViewInterface
     {
+        void NewCalendar(); // Creates a new calendar for user
+        void OpenExistingCalendar(); // Opens an existing Calendar File
+        void AddNewEvent(); // Add New Event to Calendar
+        void AddNewCategory(); // Add New Category to Calendar
+        void ShowError(string msg); // Show errors like missing infos to add
     }
 }
