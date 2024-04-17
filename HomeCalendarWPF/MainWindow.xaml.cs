5a3c00e1565ad386b973f2c774066598e96ab6ea
Halde <2256375@johnabbottcollege.net>
2024-04-17 10:46:59 -0400

    Here you go eric

--- HomeCalendarWPF/MainWindow.xaml.cs
+++ HomeCalendarWPF/MainWindow.xaml.cs
@@ -14,13 +14,15 @@ namespace HomeCalendarWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, ViewInterface
     {
+        private Presenter presenter;
+
         public MainWindow()
         {
             string baseDir = Environment.CurrentDirectory;
             InitializeComponent();
-            calendarFiletxb.Text = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\CalendarApp\\test.calendar")); ;
+            calendarFiletxb.Text = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\CalendarApp\\test.calendar"));
         }
         private void OpenEvent(object sender, RoutedEventArgs e)
         {
@@ -45,11 +47,33 @@ namespace HomeCalendarWPF
             {
                 string filename = fileSelector.FileName;
                 calendarFiletxb.Text = filename;
+                // Create new presenter with chosen file 
+                presenter = new Presenter(this, filename);
             }
         }
         private void Btn_Click_Change_Theme(object sender, RoutedEventArgs e)
         {
+            throw new NotImplementedException();
+        }
 
+        public void NewCalendar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddNewEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddNewCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string msg)
+        {
+            throw new NotImplementedException();
         }
     }
 }
